544ef34585adbad3866a014b1648af23b606a7af
jeremiahKiarie <jerekiarie01@gmail.com>
2025-06-24 21:47:22 +0300

    Added setup_docker.sh and setup_nvidia_drivers.sh from Pere49

---
+++ setup_nvidia_drivers.sh
@@ -0,0 +1,18 @@
+# Base installer for cuda toolkit
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-5
+
+# Driver installer
+sudo apt-get install -y nvidia-driver-555-open
+sudo apt-get install -y cuda-drivers-555
+
+## nvidia container toolkit for docker
+sudo apt install curl
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt install -y nvidia-container-toolkit
+
